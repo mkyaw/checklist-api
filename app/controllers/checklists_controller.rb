beb22623b29436d7327003ccb13a6a611b289844
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-22 13:46:46 -0700

    Add code to checklists controller to pass all the failing tests; Add ExceptionHandler and Response helpler modules and include them in ApplicationController to take care of the controller methods gracefully

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,2 +1,43 @@
 class ChecklistsController < ApplicationController
+  before_action :set_checklist, only: [:show, :update, :destroy]
+
+  # GET /checklists
+  def index
+    @checklists = Checklist.all
+    json_response(@checklists)
+  end
+
+  # POST /checklists
+  def create
+    @checklist = Checklist.create!(checklist_params)
+    json_response(@checklist, :created)
+  end
+
+  # GET /checklists/:id
+  def show
+    json_response(@checklist)
+  end
+
+  # PUT /checklists/:id
+  def update
+    @checklist.update(checklist_params)
+    head :no_content
+  end
+
+  # DELETE /checklists/:id
+  def destroy
+    @checklist.destroy
+    head :no_content
+  end
+
+  private
+
+  def checklist_params
+    # whitelist aprams
+    params.permit(:title, :created_by)
+  end
+
+  def set_checklist
+    @checklist = Checklist.find(params[:id])
+  end
 end
