cbece6af9720c21875d888e8f11a1e7e388d3576
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-23 09:51:24 -0700

    Generate User model; Add user specs; Update User model to pass all the failing tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_190515) do
+ActiveRecord::Schema.define(version: 2018_10_23_160225) do
 
   create_table "checklists", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2018_10_22_190515) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["checklist_id"], name: "index_items_on_checklist_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
