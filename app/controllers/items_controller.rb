fefb1035b9b21e7405646d6e27b8d25f02706245
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-22 18:46:42 -0700

    Update Items controller with all the methods and required private methods to be able to pass the specs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,46 @@
 class ItemsController < ApplicationController
+  before_action :set_checklist
+  before_action :set_checklist_item, only: [:show, :update, :destroy]
+
+  # GET /checklist/:checklist_id/items
+  def index
+    json_response(@checklist.items)
+  end
+
+  # GET /checklist/:checklist_id/items/:id
+  def show
+    json_response(@item)
+  end
+
+  # POST /checklist/:checklist_id/items
+  def create
+    @checklist.items.create!(item_params)
+    json_response(@checklist, :created)
+  end
+
+  # PUT /checklist/:checklist_id/items/:id
+  def update
+    @item.update(item_params)
+    head :no_content
+  end
+
+  # DELETE /checklist/:checklist_id/items/:id
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :done)
+  end
+
+  def set_checklist
+    @checklist = Checklist.find(params[:checklist_id])
+  end
+
+  def set_checklist_item
+    @item = @checklist.items.find_by!(id: params[:id]) if @checklist
+  end
 end
